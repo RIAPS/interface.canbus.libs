c2984e7725d6bfe04aa3355dd950624fbd09ee96
GWMAC150 <gerald.mcafee@vanderbilt.edu>
2021-08-02 22:05:25 -0500

    Simulation changes

--- CanbusSim.py
+++ CanbusSim.py
@@ -23,9 +23,10 @@ from random import random
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--CH', required=False , help="The CAN bus channel. Default = 'can0'")
     parser.add_argument('--ID', required=False, help="The CANbus message ID to publish on the bus. Default = 0x123" )
     parser.add_argument('--DATA', required=False, help="The data to send, a list in the format [1,2,3,4]. Default = [0,]" )
-    parser.add_argument('--FREQ', required=False , help="The frequency at which to send the data. Default = 1.0 sec")
+    parser.add_argument('--FREQ', required=False , help="The frequency at which to send the data. Default = 1.0 Hz")
 
     if platform.system().upper() == 'WINDOWS' :
         pass
@@ -36,26 +37,40 @@ def main():
 
     args = parser.parse_args()
 
+    if args.CH == None :
+        CH = 'can0'
+    else:
+        CH = args.CH
+
     if args.ID == None :
-        args.ID = 0x123
+        ID = 0x123
+    else:
+        ID = args.ID
 
     if args.DATA == None :
-        args.DATA = [0,]
+        DATA = [0,]
+    else:
+        DATA = args.DATA
     
     if args.FREQ == None :
-        args.FREQ = 1.0
-     
+        FREQ = 1.0
+    else:
+        FREQ = args.FREQ     
     
+    msg_period = 1E9/FREQ
+
     active = threading.Event()
     active.set()
-    
-    with open(args.config) as f:
-        data = yaml.safe_load( f )
-    
-    parms = data.keys()  
 
+    bus = can.interface.Bus(channel=CH, bustype='socketcan_native')
+    msg = can.Message( arbitration_id=ID, data=DATA )
+
+    t1 = float( time.time_ns )
     while active.is_set :
-        pass
+        t2 = float( time.time_ns )
+        if (t2 - t1) > msg_period :
+            t1 = t2
+            bus.send( msg )
 
     print( "CAN Bus simulation has exited normally." )
 
