df9f76f8112e55d2faf45b7d9576642dfdb3942b
GWMAC150 <gerald.mcafee@vanderbilt.edu>
2021-08-04 01:11:13 -0500

    Simulation testing updates

--- CanbusSim.py
+++ CanbusSim.py
@@ -11,6 +11,7 @@ Creates a local master to allow active update of some modbus parameters as neede
 from posixpath import join
 import can
 from CanbusSystemSettings import CanbusSystem
+import helper
 import sys
 import select
 import time
@@ -21,8 +22,10 @@ import os
 import platform
 import threading
 import time
+import datetime
 import queue
 from random import random
+import string
 
 RecvQueue = queue.Queue()
 SendQueue = queue.Queue()
@@ -58,7 +61,7 @@ class SendMsgThread( threading.Thread ):
             try:
                 msg = SendQueue.get( block=True, timeout=self.timing )
                 self.canbus.send( msg )
-                print( "Sent : {0}".format( msg ) )
+                print( "{0}Sent :{2} {1}".format( helper.Green, msg, helper.RESET ) )
             except queue.Empty:
                 pass
 
@@ -93,7 +96,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--CH', required=False , help="The CAN bus channel. Default = 'can0'")
     parser.add_argument('--ID', required=False, help="The CANbus message ID to publish on the bus. Default = 0x123" )
-    parser.add_argument('--DATA', required=False, help="The data to send, a list in the format [1,2,3,4]. Default = [0,]" )
+    parser.add_argument('--DATA', required=False, help="The data to send, a list in the format 1,2,3,4. Default = 0" )
     parser.add_argument('--FREQ', required=False , help="The frequency at which to send the data. Default = 1.0 Hz")
 
     if platform.system().upper() == 'WINDOWS' :
@@ -108,24 +111,29 @@ def main():
     if args.CH == None :
         CH = 'can0'
     else:
-        CH = args.CH
+        CH = str( args.CH )
 
     if args.ID == None :
-        ID = 0x043
+        ID = 0x123
     else:
-        ID = args.ID
+        ID = int( args.ID, base=16 )
 
     if args.DATA == None :
-        DATA = [0,25,0,1,3,0,150]
+        DATA = [0,]
     else:
-        DATA = args.DATA
-    
+        strs = str(args.DATA).split(",")
+        DATA = [ int(i) for i in strs ]
+
     if args.FREQ == None :
         FREQ = 1.0
     else:
-        FREQ = args.FREQ     
-    
-    msg_period = 1E9/FREQ
+        FREQ = float( args.FREQ )    
+
+    print( "CH={0}".format( CH ) )    
+    print( "ID=0x{0}".format( hex( ID )[2:].zfill(3) ) )    
+    print( "DATA={0}".format( DATA ) )    
+    print( "FREQ={0}".format( FREQ ) )    
+    send_period = 1.0/FREQ
 
     bus = can.interface.Bus(channel=CH, bustype='socketcan_native')
     msg = can.Message( arbitration_id=ID, data=DATA, extended_id=False )
@@ -137,7 +145,7 @@ def main():
     input_watcher.start()
     recv_thread.start()
     send_thread.start()
-
+    t1 = datetime.datetime.now()
     while send_thread.is_alive() or recv_thread.is_alive():
         if not input_watcher.is_alive() :
             recv_thread.Deactivate()
@@ -145,12 +153,16 @@ def main():
         else:
             try:
                 message = RecvQueue.get( block=False )
-                print( "Recv : {0}".format( message ) )
-                try:
-                    SendQueue.put( msg, block=False )
-                except queue.Full:
-                    pass
+                print( "{0}Recv :{2} {1}".format( helper.Red, message, helper.RESET ) )
             except queue.Empty:
+                pass
+
+            t2 = datetime.datetime.now()
+            try:
+                if (t2 - t1).total_seconds() > send_period :
+                    t1 = t2
+                    SendQueue.put( msg, block=False )
+            except queue.Full:
                 pass
         
     print( "CAN Bus simulation has exited normally." )
--- helper.py
+++ helper.py
@@ -1,18 +1,36 @@
 #code to color terminal text color
-
-grey = "\x1b[38;21m"
-yellow = "\x1b[33;21m"
-red = "\x1b[31;21m"
-bold_red = "\x1b[31;1m"
-reset = "\x1b[0m"
+import platform
 
 
-Red = "\033[91m"
-Green = "\033[92m"
-Yellow = "\033[93m"
-LightPurple = "\033[94m"
-Purple = "\033[95m"
-Cyan = "\033[96m"
-LightGray = "\033[97m"
-Black = "\033[98m"
-RESET = "\033[00m"
+if platform.system().upper() == 'WINDOWS' :
+    Red = "\033[38;2;255;0;0m"
+    Green = "\033[38;2;0;255;0m"
+    Yellow = "\033[38;2;255,215,0m"
+    # LightPurple = "\033[94m"
+    # Purple = "\033[95m"
+    # Cyan = "\033[96m"
+    # LightGray = "\033[97m"
+    Black = "\033[38;2;0,0,0m"
+    RESET = "\033[0m"
+elif platform.system().upper() == 'LINUX' :
+    Red = "\033[91m"
+    Green = "\033[92m"
+    Yellow = "\033[93m"
+    # LightPurple = "\033[94m"
+    # Purple = "\033[95m"
+    # Cyan = "\033[96m"
+    # LightGray = "\033[97m"
+    Black = "\033[98m"
+    RESET = "\033[00m"
+else:
+    Red = ""
+    Green = ""
+    Yellow = ""
+    # LightPurple = "\033[94m"
+    # Purple = "\033[95m"
+    # Cyan = "\033[96m"
+    # LightGray = "\033[97m"
+    # Black = "\033[98m"
+    RESET = "\033[0m"
+
+
