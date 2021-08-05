d5ea82b4aea1732ba24345ddbcb48d54ad55b60b
GWMAC150 <gerald.mcafee@vanderbilt.edu>
2021-08-04 20:02:28 -0500

    Readme updates

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# interface.canbus.libs
+# CAN Device simulation
+
+### Required Raspberry Pi3 
+### PiCAN2 module attached and connected to an operational CAN bus
+
+### Complete all updates and upgrades
+
+    $ sudo apt-get update
+    $ sudo apt-get upgrade
+    $ pip3 install python-can
+
+
+### Edit /boot/firmware/usercfg.txt 
+   
+    $ sudo nano /boot/firmware/usercfg.txt
+
+    Add the following lines:
+
+    dtparam=spi=on
+    dtoverlay=mcp2515-can0,oscillator=16000000,interrupt=25
+    dtoverlay=spi-bcm2835-overlay  
+
+### Reboot
+
+    $ sudo reboot
+
+### After restart bring up the CAN interface
+
+    $ sudo ip link set can0 up type can bitrate 500000  
+
+### Run CanbuSim.py   
+
+    $ python3 CanbusSim.py --CH can0 --ID 0x043 --DATA 0,25,0,1,3,0,150 --FREQ 1.0 --IDLIST 0x044
+
+    --CH = The can channel (device).
+    --ID = The message ID to publish on the CAN bus.
+    --DATA = List of data bytes to send with the message ID.
+    --FREQ = The frequency at which to publish the message.
+    --IDLIST = Filter for messages IDs that should be printed.
+
--- helper.py
+++ helper.py
@@ -1,36 +1,36 @@
-#code to color terminal text color
 import platform
 
+#code to color terminal text color
 
 if platform.system().upper() == 'WINDOWS' :
     Red = "\033[38;2;255;0;0m"
     Green = "\033[38;2;0;255;0m"
     Yellow = "\033[38;2;255,215,0m"
-    # LightPurple = "\033[94m"
-    # Purple = "\033[95m"
-    # Cyan = "\033[96m"
-    # LightGray = "\033[97m"
+    LightPurple = "\033[38;2;204,0,204m"
+    Purple = "\033[38;2;153,0,153m"
+    Cyan = "\033[38;2;0,255,255m"
+    LightGray = "\033[38;2;192,192,192m"
     Black = "\033[38;2;0,0,0m"
     RESET = "\033[0m"
 elif platform.system().upper() == 'LINUX' :
     Red = "\033[91m"
     Green = "\033[92m"
     Yellow = "\033[93m"
-    # LightPurple = "\033[94m"
-    # Purple = "\033[95m"
-    # Cyan = "\033[96m"
-    # LightGray = "\033[97m"
+    LightPurple = "\033[94m"
+    Purple = "\033[95m"
+    Cyan = "\033[96m"
+    LightGray = "\033[97m"
     Black = "\033[98m"
     RESET = "\033[00m"
 else:
     Red = ""
     Green = ""
     Yellow = ""
-    # LightPurple = "\033[94m"
-    # Purple = "\033[95m"
-    # Cyan = "\033[96m"
-    # LightGray = "\033[97m"
-    # Black = "\033[98m"
-    RESET = "\033[0m"
+    LightPurple = ""
+    Purple = ""
+    Cyan = ""
+    LightGray = ""
+    Black = ""
+    RESET = ""
 
 
