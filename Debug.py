473bc090f80f9fe98a956529509051a166345844
GWMAC150 <gerald.mcafee@vanderbilt.edu>
2021-09-22 23:45:50 -0500

    Updates to messages

--- Debug.py
+++ Debug.py
@@ -2,18 +2,26 @@ import libs.Terminal as tc
 import spdlog
 
 
-def debug( logger, message, level=spdlog.LogLevel.CRITICAL ) :
+def debug( logger, message, level=spdlog.LogLevel.CRITICAL, color=None ) :
     if level == spdlog.LogLevel.CRITICAL :
         logger.critical( f"{tc.Red}{message}{tc.RESET}" ) 
     elif level == spdlog.LogLevel.ERR :
         logger.err( f"{tc.Yellow}{message}{tc.RESET}" ) 
     elif level == spdlog.LogLevel.WARN :
+        if color == None:
+            color = tc.Orange
         logger.info( f"{tc.Purple}{message}{tc.RESET}" ) 
     elif level == spdlog.LogLevel.INFO :
-        logger.debug( f"{tc.Green}{message}{tc.RESET}" ) 
+        if color == None:
+            color = tc.Green
+        logger.debug( f"{color}{message}{tc.RESET}" ) 
     elif level == spdlog.LogLevel.DEBUG :
-        logger.trace( f"{tc.Cyan}{message}{tc.RESET}" ) 
+        if color == None:
+            color = tc.Cyan
+        logger.trace( f"{color}{message}{tc.RESET}" ) 
     elif level == spdlog.LogLevel.TRACE :
-        logger.trace( f"{tc.LightGray}{message}{tc.RESET}" ) 
+        if color == None:
+            color = tc.LightGray
+        logger.trace( f"{color}{message}{tc.RESET}" ) 
     else:
         logger.trace( message ) 
